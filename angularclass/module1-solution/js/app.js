735baf7a0a2ba989155dfb63cc5250b529fd0039
shakrunk <denkrishna+github@gmail.com>
2020-12-26 18:01:34 -0700

    Color chages for valid / invalid inputs

--- angularclass/module1-solution/js/app.js
+++ angularclass/module1-solution/js/app.js
@@ -11,17 +11,21 @@ function LunchCheckController($scope) {
 
   $scope.clickthebutton = function () {
     if ($scope.input == ""){
-      console.log("List empty");
       $scope.message = "Please enter data first";
+      $scope.font = "invalid";
+      console.log("List empty");
     }else if ($scope.input.split(',').length <= 3) {
-      console.log("'enjoy' || <= 3");
       $scope.message = "Enjoy!";
+      $scope.font = "valid";
+      console.log("'enjoy' || <= 3");
     }else if ($scope.input.split(',').length > 3){
-      console.log("'too much' || > 3");
       $scope.message = "Too Much!";
+      $scope.font = "valid";
+      console.log("'too much' || > 3");
     }else {
-      console.log("An unexpected error ocurred.");
       $scope.message = "An unexpected error ocurred.";
+      $scope.font = "invalid";
+      console.log("An unexpected error ocurred.");
     }
   };
 }
