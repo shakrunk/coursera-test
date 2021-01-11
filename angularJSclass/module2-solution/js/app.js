90b8c06dddca34c8e28194ee2af1a3de326c2f7e
shakrunk <denkrishna+github@gmail.com>
2021-01-11 14:44:31 -0700

    adder service function

--- angularJSclass/module2-solution/js/app.js
+++ angularJSclass/module2-solution/js/app.js
@@ -2,5 +2,26 @@
 'use strict';
 
 angular.module('ShoppingListCheckOff', [])
+.controller("ToBuyController", ToBuyController)
+.controller("AlreadyBoughtController", AlreadyBoughtController)
+.service("ShoppingListCheckOffService", ShoppingListCheckOffService);
+
+ToBuyController.$inject = ["ShoppingListCheckOffService"];
+function ToBuyController(ShoppingListCheckOffService) {
+    var buy = this;
+}
+
+ToBuyController.$inject = ["ShoppingListCheckOffService"];
+function AlreadyBoughtController(SheppingListService) {
+    var bought = this;
+}
+
+function ShoppingListCheckOffService() {
+    var service = this;
+
+    // Item Lists
+    var toBuy = [];
+    var alreadyBought = [];
+}
 
 }());
