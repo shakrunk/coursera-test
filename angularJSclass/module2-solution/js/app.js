d7fa47d89a113019e0d41e29cd81e6afb858d308
shakrunk <denkrishna+github@gmail.com>
2021-01-11 16:44:18 -0700

    more comments, better toBuy List (discreet items)

--- angularJSclass/module2-solution/js/app.js
+++ angularJSclass/module2-solution/js/app.js
@@ -12,11 +12,9 @@ function ToBuyController(ShoppingListCheckOffService) {
     
     buy.list = ShoppingListCheckOffService.toBuy; // The list of items to buy
     buy.buyItem = function($index) {
-        // -- DEBUG --
-        // console.log("debug: inside buy.buyItem function || $index = " + $index);
+        // console.log("debug: inside buy.buyItem function || $index = " + $index); // -- DEBUG --
         ShoppingListCheckOffService.buyItem($index);
-        // -- DEBUG --
-        // console.log("debug: end buy.buyItem function");
+        // console.log("debug: end buy.buyItem function"); // -- DEBUG --
     }; 
 }
 
@@ -33,9 +31,9 @@ function ShoppingListCheckOffService() {
     // Item Lists
     service.toBuy = [{name: "cookies", quantity: 5},
                      {name: "cookies", quantity: 10},
-                     {name: "cookies", quantity: 10},
-                     {name: "cookies", quantity: 10},
-                     {name: "cookies", quantity: 10}];
+                     {name: "cookies", quantity: 15},
+                     {name: "cookies", quantity: 20},
+                     {name: "cookies", quantity: 25}];
 
     service.alreadyBought = [];
 
@@ -45,13 +43,14 @@ function ShoppingListCheckOffService() {
         // console.log("debug: insde service.buyItem function || index = " + index);
         // console.log("debug: service.toBuy[index].quantity = " + service.toBuy[index].quantity);
         // console.log("debug: service.toBuy[index].name = " + service.toBuy[index].name);
+        
         // -- NOTE --
         // These next few lines could have easily been replaced with the following code:
         // service.alreadyBought.push(service.toBuy(index));
         // by having the toBuy and alreadyBought item object attributes formatted the same way
         // I specifically formatted them differently because the instuctions for this assignment
         // dictated the formatting be in this way. Hence, the extra lines.
-        var reformatted = {};
+        var reformatted = {}; // objected created to contain reformatted data
         reformatted.item_quantity = service.toBuy[index].quantity; 
         reformatted.item_name = service.toBuy[index].name;
         service.alreadyBought.push(reformatted); // Adds the bought item to the bought list
